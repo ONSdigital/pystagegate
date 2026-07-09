2859f34dcbdbf4e948f63589a6080256b4de00bf
soderc <christoffer.soderberg@ons.gov.uk>
2026-07-09 17:25:24 +0100

    Add helper function for synthetic data

--- src/pystagegate/functions.py
+++ src/pystagegate/functions.py
@@ -1,3 +1,5 @@
+from sdv.single_table import GaussianCopulaSynthesizer
+from sdv.metadata import Metadata
 import pandas as pd
 import json
 
@@ -27,3 +29,23 @@ def load_config(path: str) -> dict:
     with open(path, "r") as f:
         config = json.load(f)
     return config
+
+
+def _generate_synth_df(df: pd.DataFrame, n: int) -> pd.DataFrame:
+    """
+    Return a synthetic DataFrame produced from a DataFrame reference.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame.
+        n (int): Number of rows to return.
+
+    Returns:
+        synth_df (pd.DataFrame)
+    """
+
+    metadata = Metadata.detect_from_dataframes(data={"real data": df})
+
+    synthesizer = GaussianCopulaSynthesizer(metadata)
+    synthesizer.fit(data=df)
+
+    return synthesizer.sample(num_rows=n)
