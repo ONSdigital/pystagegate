c4ebcc3cf88bf3e3219c4eee61f3bb10ac369ae3
soderc <christoffer.soderberg@ons.gov.uk>
2026-07-31 19:32:15 +0100

    Update conftest.py to match new configuration file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,22 +2,61 @@ import pytest
 
 
 @pytest.fixture
-def test_config():
+def test_pipeline_config():
     return {
-        "parameters": {"age_min": 0, "age_max": 200, "year": 2024},
-        "root": "tests/data/",
-        "lookup_tables": {"ltim_lookup": "", "las_lookup": ""},
-        "prov_fin_paths": {
-            "spring_immigration": "final_immigration.csv",
-            "spring_emigration": "final_emigration.csv",
-            "provisional_mye": "provisional.csv",
-            "provisional_scot": "provisional_added.csv",
-        },
-        "sex_ratio_paths": {
-            "winter_immigration": "",
-            "winter_emigration": "",
-            "provisional_mye": "",
-        },
-        "yoy_paths": {"spring_immigration": "", "spring_emigration": ""},
-        "scatter_paths": {"winter_immigration": ""},
+        "prov_fin": {
+            "root_path": "tests/data/",
+            "global_parameters": {
+                "age_min": 0,
+                "age_max": 200,
+                "year": 2024,
+                "all_nationalities": "All Nationalities",
+            },
+            "datasets": {
+                "final_immigration": {
+                    "path": "final_immigration.csv",
+                    "variables": {
+                        "la_code": "Local Authority Code",
+                        "age": "Age",
+                        "sex": "Sex",
+                        "nationality": "Nationality Group",
+                        "year": "Year",
+                        "count": "Count",
+                    },
+                },
+                "final_emigration": {
+                    "path": "final_emigration.csv",
+                    "variables": {
+                        "la_code": "Local Authority Code",
+                        "age": "Age",
+                        "sex": "Sex",
+                        "nationality": "Nationality Group",
+                        "year": "Year",
+                        "count": "Count",
+                    },
+                },
+                "provisional": {
+                    "path": "provisional.csv",
+                    "variables": {
+                        "la_code": "code",
+                        "age": "Age",
+                        "sex": "sex",
+                        "immigration_prefix": "international_in_",
+                        "emigration_prefix": "international_out_",
+                        "net_prefix": "international_net_",
+                    },
+                },
+                "provisional_scot": {
+                    "path": "provisional_added.csv",
+                    "variables": {
+                        "la_code": "ca_code",
+                        "age": "Age",
+                        "sex": "sex",
+                        "direction": "dir",
+                        "year": "year",
+                        "count": "count",
+                    },
+                },
+            },
+        }
     }
