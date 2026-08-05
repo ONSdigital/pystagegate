2dbf90e3375e69e1553dd16174f8e7d5deaca351
soderc <christoffer.soderberg@ons.gov.uk>
2026-08-05 15:49:21 +0100

    Command line args for project script entry point

--- src/pystagegate/__init__.py
+++ src/pystagegate/__init__.py
@@ -1,5 +1,12 @@
+import argparse
 from pystagegate.pipelines import prov_fin_main
 
 
-def main() -> None:
-    prov_fin_main()
+def run_prov_fin() -> None:
+    parser = argparse.ArgumentParser(
+        description="Run provisional-final migration quality assurance pipeline"
+    )
+    parser.add_argument("config", type=str, help="Path to the config file")
+    args = parser.parse_args()
+
+    prov_fin_main(args.config)
