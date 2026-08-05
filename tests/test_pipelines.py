87a622a09be64b97b85f675008f3b30490c2780b
soderc <christoffer.soderberg@ons.gov.uk>
2026-08-05 10:02:44 +0100

    add test for json config

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,5 +1,9 @@
 from pystagegate import pipelines
 
 
-def test_prov_fin(test_pipeline_config):
+def test_prov_fin_dict(test_pipeline_config):
     pipelines.prov_fin_main(test_pipeline_config)
+
+
+def test_prov_fin_path():
+    pipelines.prov_fin_main("tests/data/testing_config.json")
