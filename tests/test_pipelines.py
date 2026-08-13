a7ca75f2ec76be101e6ee5093a796bc4e9ee783d
soderc <christoffer.soderberg@ons.gov.uk>
2026-08-13 15:54:07 +0100

    Small rename

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,8 +1,8 @@
 from pystagegate.pipelines import prov_fin_main
 
 
-def test_prov_fin_dict(test_pipeline_config):
-    prov_fin_main(test_pipeline_config)
+def test_prov_fin_dict(test_config):
+    prov_fin_main(test_config)
 
 
 def test_prov_fin_path():
