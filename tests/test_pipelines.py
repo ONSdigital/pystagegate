5aee62394fa4ccbdbf0e26c1eb62565c5f618127
soderc <christoffer.soderberg@ons.gov.uk>
2026-08-20 19:16:10 +0100

    formatting

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -6,7 +6,9 @@ from pystagegate.pipelines import prov_fin_main
 def test_prov_fin_dict(test_config):
     output = prov_fin_main(test_config)
 
-    expected_output = pd.read_csv(os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv"))
+    expected_output = pd.read_csv(
+        os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv")
+    )
 
     pd.testing.assert_frame_equal(output, expected_output)
 
@@ -14,6 +16,8 @@ def test_prov_fin_dict(test_config):
 def test_prov_fin_path(test_config):
     output = prov_fin_main("tests/data/testing_config.json")
 
-    expected_output = pd.read_csv(os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv"))
+    expected_output = pd.read_csv(
+        os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv")
+    )
 
     pd.testing.assert_frame_equal(output, expected_output)
