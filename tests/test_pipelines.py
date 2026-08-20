71b118d5c67e3968a7e0f25afa990170cc4daa75
soderc <christoffer.soderberg@ons.gov.uk>
2026-08-20 18:54:06 +0100

    Lock end to end testing to take a specific output

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,9 +1,19 @@
+import pandas as pd
+import os
 from pystagegate.pipelines import prov_fin_main
 
 
 def test_prov_fin_dict(test_config):
-    prov_fin_main(test_config)
+    output = prov_fin_main(test_config)
+
+    expected_output = pd.read_csv(os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv"))
+
+    pd.testing.assert_frame_equal(output, expected_output)
 
 
-def test_prov_fin_path():
-    prov_fin_main("tests/data/testing_config.json")
+def test_prov_fin_path(test_config):
+    output = prov_fin_main("tests/data/testing_config.json")
+
+    expected_output = pd.read_csv(os.path.join(test_config["prov_fin"]["root_path"], "prov_fin_output.csv"))
+
+    pd.testing.assert_frame_equal(output, expected_output)
